abaaffdf41ffd0eeaf2ae6a472df04afe441b598
Nurul-GC <nuruldecarvalho@gmail.com>
2021-07-03 03:46:50 +0200

    update commit

--- imageditor/__init__.py
+++ imageditor/__init__.py
@@ -26,8 +26,8 @@ __trademark__ = "ArtesGC Inc"
 __trade_website_ = "https://artesgc.home.blog"
 
 os.makedirs("./Debug", exist_ok=True)
-logging.basicConfig(filename=f"./Debug/{datetime.date(datetime.today())}-imagc.log", level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-logging.info('*' * 25 + ' NEW DEBUG ' + '*' * 25)
+logging.basicConfig(filename=f"./Debug/{datetime.date(datetime.today())}-imagc.log", level=logging.DEBUG, format='\n %(asctime)s - %(levelname)s - %(message)s')
+logging.info(f"{'*' * 25} NEW DEBUG {'*' * 25}")
 
 
 class ImaGC:
@@ -65,9 +65,9 @@ class ImaGC:
 
                     im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
                     im.save(os.path.join(f'{self.dir_salvar}', f"imagc-{filename}"))
-                    logging.debug(f"- Adicionando logo a imagem '{filename}'... SUCESSO!")
+                    logging.debug(f"Adicionando logotipo a imagem '{filename}'... CONCLUIDO!")
                 except Exception as erro:
-                    logging.critical(f"- {erro}..")
+                    logging.critical(f"{erro}..")
                     continue
         elif _nome_imagem and _nome_logotipo:
             SQUARE_FIT_SIZE = 100
@@ -87,11 +87,11 @@ class ImaGC:
             try:
                 im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
                 im.save(f"{self.dir_salvar}/imagc-{filename}")
-                logging.debug(f"- Adicionando logo a imagem '{filename}'... SUCESSO!")
+                logging.debug(f"Adicionando logo a imagem '{filename}'... CONCLUIDO!")
             except Exception as erro:
-                logging.critical(f"- {erro}..")
+                logging.critical(f"{erro}..")
         else:
-            logging.critical("- Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
+            logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
     def convertendoGif(self, _images: List[str]):
         """função conversora (images to gif)
@@ -100,10 +100,17 @@ class ImaGC:
         :return: nova imagem (.gif),
          salva no directorio selecionado pelo utilizador"""
         dados_imagem = []
-        for image in _images:
-            imgData = imageio.imread(image)
-            dados_imagem.append(imgData)
-        imageio.mimsave(f'{self.dir_salvar}/imagc.gif', dados_imagem, duration=1.0)
+        if _images:
+            try:
+                for image in _images:
+                    imgData = imageio.imread(image)
+                    dados_imagem.append(imgData)
+                imageio.mimsave(f'{self.dir_salvar}/imagc.gif', dados_imagem, duration=1.0)
+                logging.debug(f"Criando o arquivo '{self.dir_salvar}/imagc.gif'.. CONCLUIDO!")
+            except Exception as erro:
+                logging.critical(f"- {erro}..")
+        else:
+            logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
     def convertendoIcone(self, _size: int, _nome_imagem: str):
         """funcão conversora (image to ico)
@@ -123,34 +130,34 @@ class ImaGC:
                         for s in sz:
                             nome = f"{self.dir_salvar}/imagc-{s}x{s}.ico"
                     img_to_icon.save(nome, sizes=size)
-                    logging.debug(f"- Criando o icone '{nome}'.. SUCESSO!")
+                    logging.debug(f"Criando o icone '{nome}'.. CONCLUIDO!")
                 elif _size == 32:
                     size = SIZES[1]
                     for sz in size:
                         for s in sz:
                             nome = f"{self.dir_salvar}/imagc-{s}x{s}.ico"
                     img_to_icon.save(nome, sizes=size)
-                    logging.debug(f"- Criando o icone '{nome}'.. SUCESSO!")
+                    logging.debug(f"Criando o icone '{nome}'.. CONCLUIDO!")
                 elif _size == 64:
                     size = SIZES[2]
                     for sz in size:
                         for s in sz:
                             nome = f"{self.dir_salvar}/imagc-{s}x{s}.ico"
                     img_to_icon.save(nome, sizes=size)
-                    logging.debug(f"- Criando o icone '{nome}'.. SUCESSO!")
+                    logging.debug(f"Criando o icone '{nome}'.. CONCLUIDO!")
                 elif _size == 256:
                     size = SIZES[3]
                     for sz in size:
                         for s in sz:
                             nome = f"{self.dir_salvar}/imagc-{s}x{s}.ico"
                     img_to_icon.save(nome, sizes=size)
-                    logging.debug(f"- Criando o icone '{nome}'.. SUCESSO!")
+                    logging.debug(f"Criando o icone '{nome}'.. CONCLUIDO!")
                 else:
                     pass
             except Exception as erro:
                 logging.critical(f"- {erro}..")
         else:
-            logging.critical("- Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
+            logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
     def convertendoPdf(self, _images: List[str]):
         """função conversora (images to pdf)
@@ -158,10 +165,17 @@ class ImaGC:
         :param _images: lista de imagens
         :return: novo documento (.pdf) contendo a imagem(ns) selecionada(s),
          salva no directorio selecionado pelo utilizador"""
-        for image in _images:
-            self.pdf.add_page('P')
-            self.pdf.image(image, x=0, y=0, w=1080, h=1920)
-        self.pdf.output(f'{self.dir_salvar}/imagc.pdf', 'F')
+        if _images:
+            try:
+                for image in _images:
+                    self.pdf.add_page('P')
+                    self.pdf.image(image, w=1080, h=1920)
+                self.pdf.output(f'{self.dir_salvar}/imagc.pdf', 'F')
+                logging.debug(f"Criando o arquivo '{self.dir_salvar}/imagc.pdf'.. CONCLUIDO!")
+            except Exception as erro:
+                logging.critical(f"{erro}..")
+        else:
+            logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
     def dimensaoImagem(self, _filename: str):
         """identifica as dimensões da imagem
@@ -182,15 +196,15 @@ class ImaGC:
             try:
                 resize_img = Image.open(_nome_imagem)
                 width, heigth = self.dimensaoImagem(_nome_imagem)
-                SIZES = [(width/_resizer, heigth/_resizer)]
+                SIZES = [(int(width/_resizer), int(heigth/_resizer))]
                 for size in SIZES[0]:
-                    nome = f"{self.dir_salvar}/imagc-{size}x{size}-{_nome_imagem}"
+                    nome = f"{self.dir_salvar}/imagc-{_nome_imagem.split('/')[-1]}"
                 resize_img.save(nome, sizes=SIZES)
-                logging.debug(f"- Criando o icone '{nome}'.. SUCESSO!")
+                logging.debug(f"Redimensionando a imagem '{nome}'.. CONCLUIDO!")
             except Exception as erro:
-                logging.critical(f"- {erro}..")
+                logging.critical(f"{erro}..")
         else:
-            logging.critical("- Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
+            logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
     def tamanhoImagem(self, _filename: str):
         """calcula a quantidade de bytes da imagem
