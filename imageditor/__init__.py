fb71d22fa27ebd33a09195ff98a4c36c7eabc080
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-23 07:39:52 +0200

    update imageditor

--- imageditor/__init__.py
+++ imageditor/__init__.py
@@ -11,36 +11,36 @@
 
 import logging
 import os
-from datetime import datetime
+from datetime import date
 from typing import List
 import imageio
-from PIL import Image
+import PIL
 from fpdf import FPDF
 
 __AUTHOR__ = "Nurul Carvalho"
 __EMAIL__ = "nuruldecarvalho@gmail.com"
 __GITHUB_PROFILE__ = "https://github.com/Nurul-GC"
-__VERSION__ = "0.6-092021"
+__VERSION__ = "0.7-102021"
 __COPYRIGHT__ = "© 2021 Nurul-GC"
 __TRADEMARK__ = "ArtesGC Inc"
 __TRADE_WEBSITE_ = "https://artesgc.home.blog"
 
 os.makedirs("./Debug", exist_ok=True)
-logging.basicConfig(filename=f"./Debug/{datetime.date(datetime.today())}-imagc.log",
+logging.basicConfig(filename=f".debug/{date.today()}-imagc.log",
                     level=logging.DEBUG, format='\n %(asctime)s - %(levelname)s - %(message)s')
 logging.info(f"{'*' * 25} NEW DEBUG {'*' * 25}")
 
 
-def dimensaoImagem(_filename: str):
+def dimensao_imagem(_filename: str):
     """identifica as dimensões da imagem
 
     :param _filename: nome e localização da imagem
     :return: uma tupla contendo a largura e a altura da imagem"""
-    imagem = Image.open(_filename)
+    imagem = PIL.Image.open(_filename)
     return imagem.size
 
 
-def tamanhoImagem(_filename: str):
+def tamanho_imagem(_filename: str):
     """calcula a quantidade de bytes da imagem
 
     :param _filename: nome e localização da imagem
@@ -58,7 +58,7 @@ class ImagEditor:
         self.pdf = FPDF()
         self.dir_salvar = _dir_salvar
 
-    def addLogo(self, _nome_logotipo: str, _nome_imagem: str = None, _dir_imagens: str = None):
+    def add_logo(self, _nome_logotipo: str, _nome_imagem: str = None, _dir_imagens: str = None):
         """adicionar logotipo a outra imagem
 
         :param _nome_imagem: nome e localização da imagem
@@ -68,7 +68,7 @@ class ImagEditor:
         if _dir_imagens and _nome_logotipo:
             SQUARE_FIT_SIZE = 100
             LOGO_FILENAME = _nome_logotipo
-            logoIm = Image.open(LOGO_FILENAME)
+            logoIm = PIL.Image.open(LOGO_FILENAME)
             logoWidth, logoHeight = logoIm.size
 
             if (logoWidth and logoHeight) > SQUARE_FIT_SIZE:
@@ -83,7 +83,7 @@ class ImagEditor:
                     if filename in LOGO_FILENAME:
                         raise NameError("Nome do ficheiro semelhante ao do logotipo..")
 
-                    im = Image.open(f"{_dir_imagens}/{filename}")
+                    im = PIL.Image.open(f"{_dir_imagens}/{filename}")
                     width, height = im.size
 
                     im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
@@ -91,11 +91,11 @@ class ImagEditor:
                     logging.debug(f"Adicionando logotipo a imagem '{filename}'... CONCLUIDO!")
                 except Exception as erro:
                     logging.critical(f"{erro}..")
-                    continue
+                    raise Exception(erro)
         elif _nome_imagem and _nome_logotipo:
             SQUARE_FIT_SIZE = 100
             LOGO_FILENAME = _nome_logotipo
-            logoIm = Image.open(LOGO_FILENAME)
+            logoIm = PIL.Image.open(LOGO_FILENAME)
             logoWidth, logoHeight = logoIm.size
 
             if (logoWidth and logoHeight) > SQUARE_FIT_SIZE:
@@ -104,19 +104,20 @@ class ImagEditor:
                 logoIm = logoIm.resize((logoWidth, logoHeight))
 
             filename = _nome_imagem
-            im = Image.open(filename)
+            im = PIL.Image.open(filename)
             width, height = im.size
 
             try:
                 im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
-                im.save(f"{self.dir_salvar}/imagc-{filename}")
+                im.save(os.path.join(f"{self.dir_salvar}/", f"imagc-{filename.split('/')[-1]}"))
                 logging.debug(f"Adicionando logo a imagem '{filename}'... CONCLUIDO!")
             except Exception as erro:
                 logging.critical(f"{erro}..")
+                raise Exception(erro)
         else:
             logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
-    def convertendoGif(self, _images: List[str]):
+    def convertendo_gif(self, _images: List[str]):
         """função conversora (images to gif)
 
         :param _images: lista de imagens
@@ -128,14 +129,15 @@ class ImagEditor:
                 for image in _images:
                     imgData = imageio.imread(image)
                     dados_imagem.append(imgData)
-                imageio.mimsave(f'{self.dir_salvar}/imagc.gif', dados_imagem, duration=1.0)
+                imageio.mimsave(f"{self.dir_salvar}/imagc.gif", dados_imagem, duration=1.0)
                 logging.debug(f"Criando o arquivo '{self.dir_salvar}/imagc.gif'.. CONCLUIDO!")
             except Exception as erro:
                 logging.critical(f"- {erro}..")
+                raise Exception(erro)
         else:
             logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
-    def convertendoIcone(self, _size: int, _nome_imagem: str):
+    def convertendo_icone(self, _size: int, _nome_imagem: str):
         """funcão conversora (image to ico)
 
         :param _nome_imagem: nome e localização da imagem
@@ -145,7 +147,7 @@ class ImagEditor:
         if _nome_imagem:
             try:
                 SIZES = [[(16, 16)], [(32, 32)], [(128, 128)], [(256, 256)]]
-                img_to_icon = Image.open(_nome_imagem)
+                img_to_icon = PIL.Image.open(_nome_imagem)
                 if _size == 16:
                     size = SIZES[0]
                     for sz in size:
@@ -178,10 +180,11 @@ class ImagEditor:
                     pass
             except Exception as erro:
                 logging.critical(f"- {erro}..")
+                raise Exception(erro)
         else:
             logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
 
-    def convertendoPdf(self, _images: List[str]):
+    def convertendo_pdf(self, _images: List[str]):
         """função conversora (images to pdf)
 
         :param _images: lista de imagens
@@ -190,7 +193,7 @@ class ImagEditor:
         if _images:
             try:
                 for image in _images:
-                    width, height = dimensaoImagem(_filename=image)
+                    width, height = dimensao_imagem(_filename=image)
                     if not image.endswith(".png") and not image.endswith(".jpg") and not image.endswith(".jpeg"):
                         raise TypeError("Formato de ficheiro não suportado..")
                     if width > height:
@@ -203,26 +206,6 @@ class ImagEditor:
                 logging.debug(f"Criando o arquivo '{self.dir_salvar}/imagc.pdf'.. CONCLUIDO!")
             except Exception as erro:
                 logging.critical(f"{erro}..")
-        else:
-            logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
-
-    def redimensionarImagem(self, _resizer: float, _nome_imagem: str):
-        """função redimensionadora
-
-        :param _resizer: faça (n/100) para calcurar a percentagem a ser reduzida
-        :param _nome_imagem: nome e localização da imagem
-        :return: uma nova imagem redimensionada ou uma mensagem de erro gravada no arquivo de debug"""
-        nome = ""
-        if _nome_imagem:
-            try:
-                resize_img = Image.open(_nome_imagem)
-                width, heigth = dimensaoImagem(_nome_imagem)
-                SIZES = [(int(width/_resizer), int(heigth/_resizer))]
-                for _ in SIZES[0]:
-                    nome = f"{self.dir_salvar}/imagc-{_nome_imagem.split('/')[-1]}"
-                resize_img.save(nome, sizes=SIZES)
-                logging.debug(f"Redimensionando a imagem '{nome}'.. CONCLUIDO!")
-            except Exception as erro:
-                logging.critical(f"{erro}..")
+                raise Exception(erro)
         else:
             logging.warning("Operação Incompleta, identifique o nome e localização dos ficheiros antes de iniciar..\n")
