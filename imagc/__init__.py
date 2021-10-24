8328e31341e9239755f88dd6b59ccaa05bacfe6f
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-24 05:44:24 +0200

    update imagc

--- imagc/__init__.py
+++ imagc/__init__.py
@@ -26,9 +26,9 @@ class ImaGC:
         self.gc = QApplication(argv)
 
         # application font
-        QFontDatabase.addApplicationFont(f"{os.path.abspath(os.curdir)}/ima-fonts/lifesavers.ttf")
+        QFontDatabase.addApplicationFont("./ima-fonts/lifesavers.ttf")
 
-        img = QPixmap(f"{os.path.abspath(os.curdir)}/ima-icons/imagc.png").scaled(QSize(400, 400))
+        img = QPixmap("./ima-icons/imagc.png").scaled(QSize(400, 400))
         self.align = int(Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignAbsolute)
         self.janela = QSplashScreen(img)
         self.janela.setStyleSheet(theme)
@@ -60,6 +60,6 @@ class ImaGC:
 
 
 if __name__ == '__main__':
-    theme = open(f"{os.path.abspath(os.curdir)}/ima-themes/imagc.qss").read().strip()
+    theme = open("./ima-themes/imagc.qss").read().strip()
     gcApp = ImaGC()
     gcApp.gc.exec()
